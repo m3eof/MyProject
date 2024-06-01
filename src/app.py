474b517ddf15273d31ffdf979a1ccaa09be5bb82
m3eof <varvaramotuz027@gmail.com>
2024-06-01 14:57:15 +0300

    404

--- src/app.py
+++ src/app.py
@@ -110,6 +110,10 @@ def webinfo():
 def webnews():
     return render_template('webnews.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     with app.app_context():
